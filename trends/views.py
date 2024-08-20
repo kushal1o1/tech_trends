6ea17ffaccdacceb71bd741ea910bab2b2419a23
Kushal Baral <136962554+kushal1o1@users.noreply.github.com>
2024-08-20 17:59:22 +0545

    api Filtering through category

--- trends/views.py
+++ trends/views.py
@@ -5,3 +5,9 @@ from .serializers import TechNewsSerializer
 class TechNewsViewSet(viewsets.ModelViewSet):
     queryset = TechNews.objects.all()
     serializer_class = TechNewsSerializer
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        category = self.request.query_params.get('category', None)
+        if category:
+            queryset = queryset.filter(category=category)
+        return queryset
