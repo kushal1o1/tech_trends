800c49640c8575d4a2ff2450d5927d07bc729f9e
Kushal Baral <136962554+kushal1o1@users.noreply.github.com>
2025-03-29 20:05:02 +0545

    Feat:Created Serializer for sources

--- trends/serializers.py
+++ trends/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
-from .models import TechNews
+from .models import TechNews, NewsSources   
 
 class TechNewsSerializer(serializers.ModelSerializer):
     class Meta:
         model = TechNews
         fields = '__all__'
+
+class NewsSourcesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = NewsSources
+        fields = '__all__'
+
