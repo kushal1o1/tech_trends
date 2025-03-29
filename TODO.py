c9924e851b1b5a6c2c8fc79ef769818bb5f3d0f6
Kushal Baral <136962554+kushal1o1@users.noreply.github.com>
2025-03-29 20:51:06 +0545

    TODO: Correct TODOs

--- TODO.py
+++ TODO.py
@@ -1,18 +1,18 @@
 # TODO: Create a Subscriber and preferences to send Emails using the Gmail API and Schedule time using Django-celery beat 
-done=False
+done=True
 
 #TODO:create a api to get the post request from the subscriber and save the data in the database with category 
 done=True
 #TODO: UPDATE Category api too
 
 # TODO: Create a Celery task to send the email to the subscriber with the news of the day
-done=False
+done=True
 
 # TODO:Provide a feature to the subscriber to unsubscribe from the email list by sending a reset email:API
 done=True
 
 # TODO: Create a Django admin command to send the email to the subscriber
-done=False
+done=True
 
 #TODO:Create a Frontend REact for these features
 done=True
